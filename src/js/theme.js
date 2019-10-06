39becdaff68ed9ff16084da79d6b46596af82c7c
Igor Kukobat <igor.kukobat@playrix.rs>
2019-10-06 22:42:46 +0200

    Finished

--- src/js/theme.js
+++ src/js/theme.js
@@ -9,31 +9,39 @@ var theme = {
 	init: function() {
 		this.protectLinks();
 		this.touchHelper();
-		const buttonHover = $('.uk-button');
-		const toggleBtn = $('.js-toggle-nav');
-		const nav = $('.nav-wrap');
-		const classActive = 'uk-navbar-toggle--active';
-		const dropdownBtn = $('.js-toggle-dropdown');
-		const dropdownActive = 'dropdown--active';
-		const dropdownContainer = $('.uk-navbar-dropdown');
-		const equalItem = $('.js-height-match');
-		const fixedBckg = $('.members-section');//za paralax mozes iskoristiti
-		const introSlider = $('.js-intro-slider');
-		const introCard = $('.uk-slider-items li');
-		const introSliderArrowPrev = $('.js-slider-arrow-next');
-		const introSliderArrowNext = $('.js-slider-arrow-prev');
-		const recentStoryLink = $('.js-toggle-txt');
-		const mapBtn = $('.js-button-map');
-		const destinationCard = $('.destination-card');
-		const destinationsBtnParent = $('.js-destination-members li');
-		const mapsBtnParent = $('.js-destination-map li');
-		const closeCardBtn = $('.js-card-close');
-		const cardBtnClassActive = 'active';
+		const buttonHover = $(".uk-button");
+		const toggleBtn = $(".js-toggle-nav");
+		const nav = $(".nav-wrap");
+		const classActive = "uk-navbar-toggle--active";
+		const dropdownBtn = $(".js-toggle-dropdown");
+		const dropdownActive = "dropdown--active";
+		const fixedBckg = $(".members-section"); //za paralax mozes iskoristiti
+		const introSlider = $(".js-intro-slider");
+		const introCard = $(".js-intro-slider .uk-slider-items li");
+		const introSliderArrowPrev = $(".js-intro-arrow-next");
+		const introSliderArrowNext = $(".js-intro-arrow-prev");
+		const recentSlider = $(".js-recent-slider");
+		const recentCard = $(".js-recent-slider .uk-slider-items li");
+		const recentCardActive = "recent-card--active";
+		const recentSliderArrowPrev = $(".js-recent-arrow-next");
+		const recentSliderArrowNext = $(".js-recent-arrow-prev");
+		const mapBtn = $(".js-button-map");
+		const destinationCard = $(".destination-card");
+		const destinationsBtnParent = $(".js-destination-members li");
+		const mapsBtnParent = $(".js-destination-map li");
+		const closeCardBtn = $(".js-card-close");
+		const cardBtnClassActive = "active";
 		if (buttonHover.length) {
 			this.btnHoverEffect(buttonHover);
 		}
 		if (mapBtn.length) {
-			this.toggleDestinationCard(mapBtn, destinationCard,cardBtnClassActive, destinationsBtnParent, mapsBtnParent);
+			this.toggleDestinationCard(
+				mapBtn,
+				destinationCard,
+				cardBtnClassActive,
+				destinationsBtnParent,
+				mapsBtnParent
+			);
 		}
 		if (closeCardBtn.length) {
 			this.closeDestinationCard(closeCardBtn);
@@ -41,54 +49,55 @@ var theme = {
 		if (toggleBtn.length) {
 			this.toggleNav(toggleBtn, classActive, nav);
 		}
-		if (introCard.length) {
-			this.calcIntroSliderWidth(
-				introCard,
-				introSlider,
-				introSliderArrowPrev,
-				introSliderArrowNext
-			);
-		}
-		if (recentStoryLink.length) {
-			this.slideText(recentStoryLink);
-		}
 		if (dropdownBtn.length) {
 			this.toggleDropdown(dropdownBtn, dropdownActive);
 		}
-		if (dropdownContainer.length) {
-			this.toggleDopdownOut(
-				dropdownContainer,
-				dropdownBtn,
-				dropdownActive
+		if (introCard.length) {
+			this.calcSliderWidth(
+				introCard,
+				introSlider,
+				introSliderArrowPrev,
+				introSliderArrowNext,
+				4
 			);
 		}
-		if (dropdownContainer.length) {
-			this.equalHeight(equalItem);
-		}
-		if (navigator.userAgent.match(/Trident\/7\./) && fixedBckg.length) {
-			$('body').on('mousewheel', function() {
-				event.preventDefault();
-
-				var wheelDelta = event.wheelDelta;
-				var currentScrollPosition = window.pageYOffset;
-				window.scrollTo(0, currentScrollPosition - wheelDelta);
-			});
+		if (recentCard.length) {
+			this.calcSliderWidth(
+				recentCard,
+				recentSlider,
+				recentSliderArrowPrev,
+				recentSliderArrowNext,
+				3
+			);
+			if (!$(".touch").length) {
+				this.recentCardHover(recentCard, recentCardActive);
+			}
 		}
 	},
 
-	btnHoverEffect: function(buttonHover){
-		buttonHover.on('mouseenter', function(e) {
-			var parentOffset = $(this).offset(),
-      		relX = e.pageX - parentOffset.left,
-      		relY = e.pageY - parentOffset.top;
-			$(this).find('.uk-button-hover').css({top:relY, left:relX})
-    	}).on('mouseout', function(e) {
-			var parentOffset = $(this).offset(),
-      		relX = e.pageX - parentOffset.left,
-      		relY = e.pageY - parentOffset.top;
-    	$(this).find('.uk-button-hover').css({top:relY, left:relX})
-    	});
-  		$('[href="#"]').click(function(){return false});
+	btnHoverEffect: function(buttonHover) {
+		if ($(".touch").length) {
+			buttonHover
+				.on("mouseenter", function(e) {
+					var parentOffset = $(this).offset(),
+						relX = e.pageX - parentOffset.left,
+						relY = e.pageY - parentOffset.top;
+					$(this)
+						.find(".uk-button-hover")
+						.css({ top: relY, left: relX });
+				})
+				.on("mouseout", function(e) {
+					var parentOffset = $(this).offset(),
+						relX = e.pageX - parentOffset.left,
+						relY = e.pageY - parentOffset.top;
+					$(this)
+						.find(".uk-button-hover")
+						.css({ top: relY, left: relX });
+				});
+			$('[href="#"]').click(function() {
+				return false;
+			});
+		}
 	},
 
 	equalHeight: function(equalItem) {
@@ -98,69 +107,95 @@ var theme = {
 				itemHeight = $(this).outerHeight();
 			}
 		});
-		equalItem.css('min-height', itemHeight);
+		equalItem.css("min-height", itemHeight);
 	},
 
-	calcIntroSliderWidth: function(
-		introCard,
-		introSlider,
-		introSliderArrowPrev,
-		introSliderArrowNext
+	calcSliderWidth: function(
+		card,
+		slider,
+		sliderArrowPrev,
+		sliderArrowNext,
+		multipleNum
 	) {
-		const cardWidth = introCard.width();
-		const arrowWidth = introSliderArrowPrev.width();
+		const cardWidth = card.width();
+		const arrowWidth = sliderArrowPrev.width();
 		const cardPartWidth = cardWidth / 5;
 		const sliderWidth = cardPartWidth + $(window).width();
-		introSlider.css('width', sliderWidth);
-		introSliderArrowPrev.css(
-			'left',
-			cardWidth * 4 + cardPartWidth + arrowWidth
-		);
-		introSliderArrowNext.css(
-			'left',
-			cardWidth * 4 + cardPartWidth + arrowWidth * 2
-		);
+		slider.css("width", sliderWidth);
+		const cardWidthArrows = card.width();
+		let multipleNumber = multipleNum;
+		if (window.matchMedia("screen and (min-width: 1200px)").matches) {
+			multipleNumber = multipleNum;
+		} else if (
+			window.matchMedia("(max-width: 1199px)").matches &&
+			window.matchMedia("(min-width: 768px)").matches
+		) {
+			multipleNumber = 2;
+		} else {
+			multipleNumber = 1;
+		}
+		if (window.matchMedia("(min-width: 768px)").matches) {
+			sliderArrowPrev.css(
+				"left",
+				cardWidthArrows * multipleNumber - arrowWidth
+			);
+			sliderArrowNext.css("left", cardWidthArrows * multipleNumber);
+		} else {
+			sliderArrowPrev.css(
+				"left",
+				cardWidthArrows * multipleNumber - arrowWidth * 2
+			);
+			sliderArrowNext.css(
+				"left",
+				cardWidthArrows * multipleNumber - arrowWidth
+			);
+		}
 	},
 
-	slideText: function(recentStoryLink) {
-		recentStoryLink.hover(function() {
-			$(this)
-				.closest('li')
-				.find('p')
-				.toggleClass('closed');
-		});
-	},
-
-	toggleDestinationCard: function(mapBtn, destinationCard, cardBtnClassActive, destinationsBtnParent, mapsBtnParent) {
+	toggleDestinationCard: function(
+		mapBtn,
+		destinationCard,
+		cardBtnClassActive,
+		destinationsBtnParent,
+		mapsBtnParent
+	) {
 		mapBtn.click(function() {
-			const index = $(this).data('card') - 1;
-			destinationsBtnParent.not(destinationsBtnParent.eq(index).addClass(cardBtnClassActive)).removeClass(cardBtnClassActive);
-			mapsBtnParent.not(mapsBtnParent.eq(index).addClass(cardBtnClassActive)).removeClass(cardBtnClassActive);
+			const index = $(this).data("card") - 1;
+			destinationsBtnParent
+				.not(
+					destinationsBtnParent.eq(index).addClass(cardBtnClassActive)
+				)
+				.removeClass(cardBtnClassActive);
+			mapsBtnParent
+				.not(mapsBtnParent.eq(index).addClass(cardBtnClassActive))
+				.removeClass(cardBtnClassActive);
 			destinationCard.not(destinationCard.eq(index).show()).hide();
 		});
 	},
-	
+
 	closeDestinationCard: function(closeCardBtn) {
-		closeCardBtn.click(function(){
-			$(this).closest('.destination-card').hide();
+		closeCardBtn.click(function() {
+			$(this)
+				.closest(".destination-card")
+				.hide();
 		});
 	},
 
 	protectLinks: function() {
-		var $links = $('a[target="_blank"]');
+		var $links = $("a[target='_blank']");
 		if (!$links.length) return;
 
 		$links.each(function() {
 			var $a = $(this),
-				rel = $a.attr('rel'),
-				protect = ['noopener', 'noreferrer'];
+				rel = $a.attr("rel"),
+				protect = ["noopener", "noreferrer"];
 
-			rel = !$nb.x(rel) ? rel.split(' ') : [];
+			rel = !$nb.x(rel) ? rel.split(" ") : [];
 			for (var i = 0; i < protect.length; i++) {
 				if (rel.indexOf(protect[i]) < 0) rel.push(protect[i]);
 			}
 
-			$a.attr('rel', rel.join(' '));
+			$a.attr("rel", rel.join(" "));
 		});
 	},
 
@@ -190,12 +225,9 @@ var theme = {
 		});
 	},
 
-	toggleDopdownOut: function(obj, dropdownBtn, classActive) {
-		$(document).mouseup(function(e) {
-			if (!obj.is(e.target) && obj.has(e.target).length === 0) {
-				dropdownBtn.parent().removeClass(classActive);
-				obj.hide();
-			}
+	recentCardHover: function(recentCard, classActive) {
+		recentCard.hover(function() {
+			$(this).toggleClass(classActive);
 		});
 	}
 };
